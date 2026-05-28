50fe7fecb3a570ca2da3ca134dc50f070e8a21aa
Alejandra Granizo Caballo <alejandra.granizo@alumnos.upm.es>
2026-05-28 10:20:29 +0200

    Primer commit - proyecto Merisur con Git LFS

--- UsersDataBase.sql
+++ UsersDataBase.sql
@@ -1,5 +1,3 @@
-CREATE TABLE usuarios (
-   id INT PRIMARY KEY AUTO_INCREMENT,
-   usuario VARCHAR(120) NOT NULL,
-   password VARCHAR(255) NOT NULL
-);
+version https://git-lfs.github.com/spec/v1
+oid sha256:9a89d537ab33427662c92aaff1bf5f1bacc5713bcdfe9d65cc37bdd916c5e4c1
+size 138
